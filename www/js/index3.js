af6b631793e568fac01b47d414b468417f2a68da
LeventeBito <BITL1_16@uni.worc.ac.uk>
2017-10-16 11:19:12 +0100

    more complicated stuff

--- www/js/index3.js
+++ www/js/index3.js
@@ -14,3 +14,5 @@ var getPerson = window.localStorage.getItem("address");
 var personJSON = JSON.parse(getPerson);
 
 alert(person.firstName + " is from " + person.address.city);
+
+document.write(person.firstName +" "+ person.lastName + " is from " + person.address.city + " and lives on " + person.address.streetAddress + ".");
