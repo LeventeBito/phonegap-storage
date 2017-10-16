ae9bbceab9ed1ba35e01dbd7a9461f0dc3ba98aa
LeventeBito <BITL1_16@uni.worc.ac.uk>
2017-10-16 11:07:27 +0100

    JSON test

---
+++ www/js/index3.js
@@ -0,0 +1,16 @@
+var person= {
+		"firstName": "Homer",
+  		"lastName": "Simpson",
+ 		"address": {
+    			"streetAddress": "742 Evergreen Terrace",
+    			"city": "Springfield",
+    			"state": "?"
+		}
+	}
+var personString =JSON.stringify(person);
+window.localStorage.setItem("address",personString);
+
+var getPerson = window.localStorage.getItem("address");
+var personJSON = JSON.parse(getPerson);
+
+alert(person.firstName + " is from " + person.address.city);
